578958591f273ca72f0ea02800089369c5e8191b
Gustavo Toro <gtoroap@gmail.com>
2020-10-04 11:47:21 -0500

    Implemented game creation from api request

---
+++ app/controllers/api/v1/games_controller.rb
@@ -0,0 +1,20 @@
+module Api
+  module V1
+    class GamesController < ApplicationController
+      def create
+        @game = Game.new game_params
+
+        if @game.valid?
+          @game.save
+          render(status: :ok, json: @game)
+        else
+          render(status: :bad_request, json: { message: @game.errors.full_messages })
+        end
+      end
+
+      def game_params
+        params.permit(:rows, :columns, :mines)
+      end
+    end
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,14 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
-  resources :games, only: [:index, :create, :show] do
-    resources :moves, only: [:create] do
-      collection do
-        post '/flag' => 'moves#flag'
+  namespace :api, defaults: {format: :json} do 
+    namespace :v1 do
+      resources :games, only: [:index, :create, :show] do
+        resources :moves, only: [:create] do
+          collection do
+            post '/flag' => 'moves#flag'
+          end
+        end
       end
     end
   end
