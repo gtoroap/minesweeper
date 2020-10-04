f51eb5f990065c472bd3368c7792bea055c57e50
Gustavo Toro <gtoroap@gmail.com>
2020-10-04 11:22:32 -0500

    Configure routes for games and moves

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,11 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  resources :games, only: [:index, :create, :show] do
+    resources :moves, only: [:create] do
+      collection do
+        post '/flag' => 'moves#flag'
+      end
+    end
+  end
 end
