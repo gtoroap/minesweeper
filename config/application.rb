c7cdcf9f4ca62b8a5d392cc13b4a656fe8b32cc4
Gustavo Toro <gtoroap@gmail.com>
2020-10-14 09:56:41 -0500

    Added active job

--- config/application.rb
+++ config/application.rb
@@ -3,6 +3,7 @@ require_relative 'boot'
 require 'active_record/railtie'
 require 'action_controller/railtie'
 require 'action_view/railtie'
+require 'active_job/railtie'
 require 'rails/test_unit/railtie'
 require 'sprockets/railtie'
 
